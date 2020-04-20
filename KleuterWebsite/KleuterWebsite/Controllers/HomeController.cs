4134f5f45a4115d731f92c79e1d7469a2962dd19
Yoeri <yoerisetz02@gmail.com>
2020-04-20 13:41:40 +0200

    Changed from asp.net core to web app

--- KleuterWebsite/KleuterWebsite/Controllers/HomeController.cs
+++ KleuterWebsite/KleuterWebsite/Controllers/HomeController.cs
@@ -1,43 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using KleuterWebsite.Models;
+using System.Web;
+using System.Web.Mvc;
 
 namespace KleuterWebsite.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public ActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public IActionResult Contact()
+        public ActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewBag.Message = "Your contact page.";
 
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
