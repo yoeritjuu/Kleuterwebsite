e703cef67f6d61683689c44753abc6f220a4105d
Yoeri <yoerisetz02@gmail.com>
2020-04-20 15:18:47 +0200

    add stats view with commands
    added commandcontroller, comands class in models folder and stats view, also fixed bootstrap and css files

---
+++ KleuterWebsite/KleuterWebsite/Controllers/KleuterBotController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using KleuterWebsite.Models;
+
+namespace KleuterWebsite.Controllers
+{
+    public class KleuterBotController : Controller
+    {
+        // GET: KleuterBot
+        public ActionResult Stats()
+        {
+            var command = new Command() {Name = "8Ball"};
+
+            return View(command);
+        }
+    }
+}
