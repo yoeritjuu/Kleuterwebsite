8ca9e2894c11c1bd21be4818769f86c327953ea2
Yoeri <yoerisetz02@gmail.com>
2020-04-20 12:25:24 +0200

    create project

---
+++ KleuterWebsite/KleuterWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KleuterWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
