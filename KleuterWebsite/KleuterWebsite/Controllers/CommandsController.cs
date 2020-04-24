b5ab2ccc6a32ba681f1b1f1e918542f3a5e5cccc
Yoeri <yoerisetz02@gmail.com>
2020-04-24 15:10:33 +0200

    add model, controller and view and code for query

---
+++ KleuterWebsite/KleuterWebsite/Controllers/CommandsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KleuterWebsite.Models;
+
+namespace KleuterWebsite.Controllers
+{
+    public class CommandsController : Controller
+    {
+        /*SQLiteConnection conn = new SQLiteConnection(@"Data Source=C:MySQLiteDB.s3db");
+        conn.Open();
+
+        SQLiteCommand cmd = new SQLiteCommand(conn);
+        cmd.CommandText = "select * from Customer";
+
+        SQLiteDataReader reader = cmd.ExecuteReader();
+            while (reader.Read( ))
+        {
+            // do something
+        }
+        reader.Close( );
+
+        cmd.CommandText = "delete from Customer where CustomerID = 33";
+        cmd.ExecuteScalar( );
+
+        conn.Close( );
+        */
+
+
+        // GET: Commands
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListCommands()
+        {
+            List<CommandModel> Commands = new List<CommandModel>();
+
+            Commands.Add();
+
+            return View();
+        } 
+    }
+}
---
+++ KleuterWebsite/KleuterWebsite/Models/CommandModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KleuterWebsite.Models
+{
+    public class CommandModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Owner { get; set; }
+        public int Usage { get; set; }
+
+    }
+}
