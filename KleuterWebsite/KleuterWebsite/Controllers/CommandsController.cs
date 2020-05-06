6a6b0d5c55839b042923e3cd747221d852d048ea
Yoeri <yoerisetz02@gmail.com>
2020-05-06 13:48:32 +0200

    created a hard coded table of data

--- KleuterWebsite/KleuterWebsite/Controllers/CommandsController.cs
+++ KleuterWebsite/KleuterWebsite/Controllers/CommandsController.cs
@@ -40,9 +40,11 @@ namespace KleuterWebsite.Controllers
         {
             List<CommandModel> Commands = new List<CommandModel>();
 
-            Commands.Add();
+            Commands.Add(new CommandModel {Id = 1, Name = "8ball", Owner = 1, Usage = 2});
+            Commands.Add(new CommandModel { Id = 2, Name = "ping", Owner = 0, Usage = 5 });
+            Commands.Add(new CommandModel { Id = 3, Name = "help", Owner = 0, Usage = 3 });
 
-            return View();
+            return View(Commands);
         } 
     }
 }
