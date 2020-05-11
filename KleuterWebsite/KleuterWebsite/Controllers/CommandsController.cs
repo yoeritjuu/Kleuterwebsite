d00cac2189da0a8e4bb25b67ced392874da71aa1
Yoeri <yoerisetz02@gmail.com>
2020-05-11 15:27:43 +0200

    re add bootstrap theme, add sqlite commands and other small things

--- KleuterWebsite/KleuterWebsite/Controllers/CommandsController.cs
+++ KleuterWebsite/KleuterWebsite/Controllers/CommandsController.cs
@@ -10,24 +10,26 @@ namespace KleuterWebsite.Controllers
 {
     public class CommandsController : Controller
     {
-        /*SQLiteConnection conn = new SQLiteConnection(@"Data Source=C:MySQLiteDB.s3db");
-        conn.Open();
-
-        SQLiteCommand cmd = new SQLiteCommand(conn);
-        cmd.CommandText = "select * from Customer";
-
-        SQLiteDataReader reader = cmd.ExecuteReader();
-            while (reader.Read( ))
+        public void SQLiteConn()
         {
-            // do something
+            SQLiteConnection conn = new SQLiteConnection(@"Data Source=C:MySQLiteDB.s3db");
+            conn.Open();
+
+            SQLiteCommand cmd = new SQLiteCommand(conn);
+            cmd.CommandText = "select * from Customer";
+
+            SQLiteDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                // do something
+            }
+            reader.Close();
+
+            cmd.CommandText = "delete from Customer where CustomerID = 33";
+            cmd.ExecuteScalar();
+
+            conn.Close();
         }
-        reader.Close( );
-
-        cmd.CommandText = "delete from Customer where CustomerID = 33";
-        cmd.ExecuteScalar( );
-
-        conn.Close( );
-        */
 
 
         // GET: Commands
@@ -40,11 +42,11 @@ namespace KleuterWebsite.Controllers
         {
             List<CommandModel> Commands = new List<CommandModel>();
 
-            Commands.Add(new CommandModel {Id = 1, Name = "8ball", Owner = 1, Usage = 2});
+            Commands.Add(new CommandModel { Id = 1, Name = "8ball", Owner = 1, Usage = 2 });
             Commands.Add(new CommandModel { Id = 2, Name = "ping", Owner = 0, Usage = 5 });
             Commands.Add(new CommandModel { Id = 3, Name = "help", Owner = 0, Usage = 3 });
 
             return View(Commands);
-        } 
+        }
     }
 }
